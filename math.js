f852c4d2c07900b18d9c751f47164aa7943ce901
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-20 20:37:22 +0530

    module /import /exports in node with multiple functions

--- math.js
+++ math.js
@@ -1,4 +1,10 @@
 function sum(a,b){
     return a+b;
 }
-module.exports=sum;
+function sub(a,b){
+    return a-b;
+}
+module.exports={
+    sum,
+    sub,
+}
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const math =require(`./math`);
-console.log(math(3,4))
+console.log(math.sub(3,4))
+console.log(math.sum(3,4))
