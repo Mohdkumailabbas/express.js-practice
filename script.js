68fe52aede782001bfe8dc88a6fad1ff14d2b723
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-20 18:45:43 +0530

    how to use post req method

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 
-const express= require('express')
- const users =[
-  {
-     name:"john",
-     kidney:[{
-      healthy:false
-     }]
-  },
-   {
-     name:"jane",
-     kidney:[{
-      healthy:true
-     }]
-   }
-   ]
-   const app= express();
-   app.get("/john", function(req,res){
-    const johnkidney=users[0].kidney;
-    const numberofkidney =johnkidney.length;
-    let noofhealthykidney=0;
-    for (let i=0;i<johnkidney.length;i++){
-      if(johnkidney[i].healthy){
-        numberofkidney=numberofkidney+1
-      }
+const express = require('express');
+let users = [
+    {
+        name: "john",
+        kidney: [{
+            healthy: false
+        }]
+    },
+    {
+        name: "jane",
+        kidney: [{
+            healthy: true
+        }]
     }
-    const noofunhealthykidney= numberofkidney-noofhealthykidney;
+];
+
+const app = express();
+app.use(express.json());
+
+app.get("/john", function(req, res) {
+    const johnKidney = users[0].kidney;
+    const numberOfKidney = johnKidney.length;
+    let noOfHealthyKidney = 0;
+    for (let i = 0; i < johnKidney.length; i++) {
+        if (johnKidney[i].healthy) {
+            noOfHealthyKidney = noOfHealthyKidney + 1;
+        }
+    }
+    const noOfUnhealthyKidney = numberOfKidney - noOfHealthyKidney;
     res.json({
-      numberofkidney,
-      noofhealthykidney,
-      noofunhealthykidney
-    })
-   })
-   app.listen(3000);
-  
+        numberOfKidney,
+        noOfHealthyKidney,
+        noOfUnhealthyKidney
+    });
+});
+
+app.post("/john", function(req, res) {
+    const isHealthy = req.body.ishealthy;
+    users[0].kidney.push({
+        healthy: isHealthy
+    });
+    res.json({});
+});
+
+app.listen(3000);
