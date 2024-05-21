3b050d8a61cfedb6f8307af073c1c4f5b74bca13
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-21 16:39:30 +0530

    saving info using fs

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //how to make server using node js
+const { log } = require("console");
+const fs= require("fs");
 const http=require("http");
 const myserver=http.createServer((req,res)=>{
-    console.log("new req recieved");
-    res.end("hello from server");
+    //saving client date when he logged
+    const log=`${Date.now()}:NEW REQ RECIEVED/n`
+    fs.appendFile("./text.txt",log, (err,data)=>{
+        console.log("new req recieved");
+        res.end("hello from server");
+    });
+    
+  
 });
 myserver.listen(3000);
