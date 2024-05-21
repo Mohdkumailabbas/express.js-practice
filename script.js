7197bad9b8300821900c9f66c7d76918d4960e69
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-21 17:01:52 +0530

    saving info acording to path and give output acc to path

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const fs= require("fs");
 const http=require("http");
 const myserver=http.createServer((req,res)=>{
     //saving client date when he logged
-    const log=`${Date.now()}:NEW REQ RECIEVED/n`
+    const log=`${Date.now()}: ${req.url} NEW REQ RECIEVEDNEW REQ RECEIVED\n`
     fs.appendFile("./text.txt",log, (err,data)=>{
         console.log("new req recieved");
-        res.end("hello from server");
+        switch(req.url){
+            case '/': res.end("homepage");
+            break
+            case '/aboutus':res.end("hello i am a webdevloper");
+            break
+            default:
+                res.end("404 not found")        
+        }
     });
     
   
