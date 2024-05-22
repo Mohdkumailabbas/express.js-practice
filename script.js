789215f384d6992c45fa4635a6d9e9f134d9ef4a
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-22 17:15:39 +0530

    use of url package (url parsing)

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 const { log } = require("console");
 const fs= require("fs");
 const http=require("http");
+const url = require("url");
 const myserver=http.createServer((req,res)=>{
     //saving client date when he logged
     const log=`${Date.now()}: ${req.url} NEW REQ RECIEVEDNEW REQ RECEIVED\n`
+    //parsing req(breaking down info)
+    const myurl=url.parse(req.url); 
+    console.log(myurl);
     fs.appendFile("./text.txt",log, (err,data)=>{
         console.log("new req recieved");
         switch(req.url){
