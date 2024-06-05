cc75575e07fe7ee6816c527b954ad315865111c6
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-06-05 13:26:42 +0530

    myfirst mongocode

--- script.js
+++ script.js
@@ -1,73 +1,141 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const jwtPassword = "123456";
-const app = express();
-app.use(express.json());
-const mongoose =require("mongoose");
-mongoose.connect("mongodb+srv://cluster0.rff7edc.mongodb.net/user_app")
-const User =mongoose.model('Users',{
-    name:"string",
-    email:"string",
-    password:"string"
-});
-// Sign-in route to generate a JWT token
-app.post("/signin", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const body = req.body.name;
-    const existinguser=await.findOne({email:username});
-     if(existinguser){
-        return res.status(404).send("username exists");
-     }
-     const user = new User ({
-        name:`Spidy`,
-        email:`spidy78@gmail.com`,
-        password:`12345678`
+const express =require("express");
+const app=express();
+const usermodel= require('./zod');
+ app.get("/",(req,res)=>{
+    res.send("hey");
+ })
+ app.get("/create",async (req,res)=>{
+     const createdata = await usermodel.create({
+        name:"aka",
+        email:"anan@gmail.com",
+        password:"12333"
+
     })
-    user.save();
-    res.json({
-        "msg":"user created succesfully"
-    })
+    res.json(createdata);
+ })
 
-    // Check if the user exists
-    // if (!userExists(username, password)) {
-    //     return res.status(403).json({
-    //         msg: "User does not exist"
-    //     });
-    // }
+ app.listen(3000);
 
-    // Generate a JWT token
-    const token = jwt.sign({ username: username }, jwtPassword);
-    return res.json({
-        token
-    });
-});
+ const mongoose =require("mongoose");
+ mongoose.connect(`mongodb://127.0.0.1:27017/practice`)
+ const UserSchema = new mongoose.Schema({
+     name:"string",
+     email:"string",
+     password:"string"
+ });
+ mongoose.model("user",UserSchema);
+ // const user = new User ({
+ //     name:`Spidy`,
+ //     email:`spidy78@gmail.com`,
+ //     password:`12345678`
+ // })
+ // user.save();
+ 
+ module.exports=mongoose.model("user",UserSchema);
 
-// Route to get all users, requires a valid JWT token
-app.get("/users", (req, res) => {
-    const token = req.headers.authorization;
+ 
 
-    try {
-        // Verify the token
-        const decoded = jwt.verify(token, jwtPassword);
-        const username = decoded.username;
 
-        // Send the list of all users
-        res.json({
-            users: allUsers
-        });
-    } catch (err) {
-        // Send an error response if the token is invalid
-        return res.status(401).json({
-            msg: "Invalid token"
-        });
-    }
-});
 
-// Start the server
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const jwtPassword = "123456";
+// const app = express();
+// app.use(express.json());
+// const mongoose =require("mongoose");
+// mongoose.connect("mongodb+srv://cluster0.rff7edc.mongodb.net/user_app")
+// const User =mongoose.model('Users',{
+//     name:"string",
+//     email:"string",
+//     password:"string"
+// });
+// // Sign-in route to generate a JWT token
+// app.post("/signin", (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     const body = req.body.name;
+//     const existinguser=await.findOne({email:username});
+//      if(existinguser){
+//         return res.status(404).send("username exists");
+//      }
+//      const user = new User ({
+//         name:`Spidy`,
+//         email:`spidy78@gmail.com`,
+//         password:`12345678`
+//     })
+//     user.save();
+//     res.json({
+//         "msg":"user created succesfully"
+//     })
+
+//     // Check if the user exists
+//     // if (!userExists(username, password)) {
+//     //     return res.status(403).json({
+//     //         msg: "User does not exist"
+//     //     });
+//     // }
+
+//     // Generate a JWT token
+//     const token = jwt.sign({ username: username }, jwtPassword);
+//     return res.json({
+//         token
+//     });
+// });
+
+// // Route to get all users, requires a valid JWT token
+// app.get("/users", (req, res) => {
+//     const token = req.headers.authorization;
+
+//     try {
+//         // Verify the token
+//         const decoded = jwt.verify(token, jwtPassword);
+//         const username = decoded.username;
+
+//         // Send the list of all users
+//         res.json({
+//             users: allUsers
+//         });
+//     } catch (err) {
+//         // Send an error response if the token is invalid
+//         return res.status(401).json({
+//             msg: "Invalid token"
+//         });
+//     }
+// });
+
+// // Start the server
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// });
 
 
 
